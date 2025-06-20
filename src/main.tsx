c0a16217135d54d300b3dccaf0e29e2bcb011776
Manoj Patidar <manojpatidar1207@gmail.com>
2025-06-20 16:22:44 +0530

    Update toaser

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import App from "@/App.tsx";
+import { Toaster } from "@/components/ui/sonner";
 
 import "@/index.css";
 
@@ -10,6 +11,7 @@ const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Toaster />
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
